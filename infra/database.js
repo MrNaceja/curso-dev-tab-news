650614ebde566b611d76bc17c3851d870351eff3
Eduardo Toriani <edutoriani13@gmail.com>
2025-11-20 00:58:41 -0300

    cria .env e conexao com bd

---
+++ infra/database.js
@@ -0,0 +1,17 @@
+import { Client } from "pg";
+
+async function query(q) {
+  const client = new Client({
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    port: process.env.POSTGRES_PORT,
+  });
+  await client.connect();
+  const result = await client.query(q);
+  await client.end();
+  return result;
+}
+
+export default { query };
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,16 @@
-export default function handle(req, res) {
-  if (req.method !== "GET") res.status(405).send();
+import database from "../../../../infra/database.js";
 
+export default async function (req, res) {
+  switch (req.method) {
+    case "GET":
+      return await GET(req, res);
+    default:
+      return res.status(405).send();
+  }
+}
+
+async function GET(req, res) {
+  const result = await database.query("select 1 + 1 as sum;");
+  console.log(result);
   res.status(200).json({ ok: true });
 }
