c0bdd0997e17c4f674b16647505fd5ff24ffc90f
Eduardo Toriani <edutoriani13@gmail.com>
2025-11-21 14:40:17 -0300

    improve ssl setup with more robust extractor

--- infra/database.js
+++ infra/database.js
@@ -1,18 +1,24 @@
 import { Client } from "pg";
 
+const useSSL = () => {
+  if (process.env.SSL_CREDENTIAL_CERTIFICATE) {
+    return {
+      ca: process.env.SSL_CREDENTIAL_CERTIFICATE,
+    };
+  }
+
+  return process.env.NODE_ENV !== "development";
+};
+
 async function query(q) {
-  const postgressCredentials = {
+  const client = new Client({
     user: process.env.POSTGRES_USER,
     host: process.env.POSTGRES_HOST,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
     port: process.env.POSTGRES_PORT,
-  };
-  const client = new Client({
-    ...postgressCredentials,
-    ssl: process.env.NODE_ENV !== "development",
+    ssl: useSSL(),
   });
-  console.info("Credenciais Postgres: ", postgressCredentials);
 
   try {
     await client.connect();
