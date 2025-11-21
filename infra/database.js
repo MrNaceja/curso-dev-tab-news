e69d4c042337dbfc0b278d84acd6c081d42d5366
Eduardo Toriani <edutoriani13@gmail.com>
2025-11-21 12:18:06 -0300

    show logs on database and improve error connection handling

--- infra/database.js
+++ infra/database.js
@@ -1,20 +1,23 @@
 import { Client } from "pg";
 
 async function query(q) {
-  const client = new Client({
+  const postgressCredentials = {
     user: process.env.POSTGRES_USER,
     host: process.env.POSTGRES_HOST,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
     port: process.env.POSTGRES_PORT,
-  });
-  await client.connect();
+  };
+  const client = new Client(postgressCredentials);
+  console.info("Credenciais Postgres: ", postgressCredentials);
 
   try {
+    await client.connect();
     const result = await client.query(q);
     return result;
   } catch (e) {
     console.error(e);
+    throw e;
   } finally {
     await client.end();
   }
