2907fd03b5c4afe0338e1048f8dfb9b9644cde41
Eduardo Toriani <edutoriani13@gmail.com>
2025-11-21 08:37:15 -0300

    finish /status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,15 +1,52 @@
-import database from "infra/database.js";
+import database from "infra/database";
 
-export default async function (req, res) {
+export default function (req, res) {
   switch (req.method) {
     case "GET":
-      return await GET(req, res);
+      return GET(req, res);
     default:
       return res.status(405).send();
   }
 }
 
 async function GET(req, res) {
-  const result = await database.query("select 1 + 1 as sum;");
-  res.status(200).json({ ok: true });
+  const { version, maxConnections, openedConnections } =
+    await catchDatabaseDependencyMetadata();
+
+  const status = {
+    updated_at: new Date().toISOString(),
+    dependencies: {
+      database: {
+        postgres_version: version,
+        max_connections: maxConnections,
+        opened_connections: openedConnections,
+      },
+    },
+  };
+
+  res.status(200).json(status);
 }
+
+const catchDatabaseDependencyMetadata = async () => {
+  const versionAndMaxConnectionsQueryResult = await database.query({
+    text: "SELECT setting FROM pg_settings WHERE name IN ('server_version', 'max_connections') ORDER BY name",
+    rowMode: "array",
+  });
+
+  const [maxConnections, version] =
+    versionAndMaxConnectionsQueryResult.rows.map(([row]) => row);
+
+  const openedConnectionsQueryResult = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1",
+    values: [process.env.POSTGRES_DB],
+    rowMode: "array",
+  });
+
+  const [openedConnections] = openedConnectionsQueryResult.rows;
+
+  return {
+    maxConnections: parseInt(maxConnections),
+    openedConnections: parseInt(openedConnections),
+    version,
+  };
+};
--- tests/integration/api/v1/status/GET.test.js
+++ tests/integration/api/v1/status/GET.test.js
@@ -1,6 +1,24 @@
-test("GET on /api/v1/status should be receive 200 status code", async () => {
+test("GET on /api/v1/status should be receive 200 status code and a valid status body fields", async () => {
   const res = await fetch("http://localhost:3000/api/v1/status", {
     method: "GET",
   });
   expect(res.status).toBe(200);
+
+  const status = await res.json();
+
+  console.info(status);
+  expect(status).toEqual(
+    expect.objectContaining({
+      updated_at: expect.stringContaining(
+        new Date(status.updated_at).toISOString(),
+      ),
+      dependencies: expect.objectContaining({
+        database: expect.objectContaining({
+          postgres_version: expect.stringContaining("16.0"),
+          max_connections: expect.any(Number),
+          opened_connections: 1,
+        }),
+      }),
+    }),
+  );
 });
