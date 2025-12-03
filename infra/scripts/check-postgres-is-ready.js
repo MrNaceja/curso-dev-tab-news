1d1d2f2063437d1df4db21f742c2204fad11cb98
Eduardo Toriani <edutoriani13@gmail.com>
2025-12-02 21:56:51 -0300

    create a check postgres status helper script

---
+++ infra/scripts/check-postgres-is-ready.js
@@ -0,0 +1,19 @@
+const { exec } = require("node:child_process");
+
+function checkPostgresIsReady() {
+  exec(
+    "docker exec local-database-postgres pg_isready --host localhost",
+    (_, stdout) => {
+      if (stdout.includes("accepting connections")) {
+        process.stdout.write("\n ✅ Serviço postgres esta pronto \n");
+        return;
+      }
+
+      process.stdout.write(".");
+      checkPostgresIsReady();
+    },
+  );
+}
+
+process.stdout.write("\n ⛔ Aguardando serviço postgres");
+checkPostgresIsReady();
