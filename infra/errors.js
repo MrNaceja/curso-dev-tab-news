af92674ac3687de4c5dbb167817861b83056dce0
Eduardo Toriani <edutoriani13@gmail.com>
2025-12-29 07:40:57 -0300

    feat: create a base api routes controller

--- infra/errors.js
+++ infra/errors.js
@@ -17,3 +17,40 @@ export class InternalServerError extends Error {
     };
   }
 }
+
+export class MethodNotAllowedError extends Error {
+  constructor() {
+    super("Método HTTP não permitido.");
+    this.name = "MethodNotAllowedError";
+    this.action =
+      "Verifique o método HTTP enviado nos cabeçalhos da requisição.";
+    this.statusCode = 405;
+  }
+
+  toJSON() {
+    return {
+      message: this.message,
+      name: this.name,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class NotFoundError extends Error {
+  constructor() {
+    super("Recurso não encontrado");
+    this.name = "NotFoundError";
+    this.action = "Verifique os cabeçalhos enviados e a formatação da URL";
+    this.statusCode = 404;
+  }
+
+  toJSON() {
+    return {
+      message: this.message,
+      name: this.name,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
