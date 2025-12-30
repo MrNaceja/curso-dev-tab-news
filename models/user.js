b3c0c2481833790b70c455f6bae69199e54f9152
Eduardo Toriani <edutoriani13@gmail.com>
2025-12-30 01:16:42 -0300

    feat: add validation for unique `email` and `password` on user

--- models/user.js
+++ models/user.js
@@ -1,7 +1,47 @@
 import database from "infra/database";
+import { ValidationError } from "infra/errors";
+
+async function validateUniqueEmail(email) {
+  const existsQuery = await database.query({
+    text: `
+     SELECT 1
+      FROM users
+      WHERE LOWER(email) = LOWER($1) 
+  `.trim(),
+    values: [email],
+  });
+
+  if (existsQuery.rowCount !== 0) {
+    throw new ValidationError({
+      message: "Email duplicado.",
+      action: "Tente outro email.",
+    });
+  }
+}
+
+async function validateUniqueUsername(username) {
+  const existsQuery = await database.query({
+    text: `
+     SELECT 1
+      FROM users
+      WHERE LOWER(username) = LOWER($1) 
+  `.trim(),
+    values: [username],
+  });
+
+  if (existsQuery.rowCount !== 0) {
+    throw new ValidationError({
+      message: "Apelido não disponível.",
+      action: "Tente outro apelido.",
+    });
+  }
+}
 
 export const User = {
   async create({ username, email, password }) {
+    await validateUniqueEmail(email);
+    await validateUniqueUsername(username);
+
     const insertQuery = await database.query({
       text: `
         INSERT INTO
