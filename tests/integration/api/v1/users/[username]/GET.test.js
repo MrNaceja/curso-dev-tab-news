bac6e961c29852729e3f65b38a6f11844565b038
Eduardo Toriani <edutoriani13@gmail.com>
2025-12-30 20:38:34 -0300

    feat: implement `api/v1/users/[username]` endpoint controller

---
+++ tests/integration/api/v1/users/[username]/GET.test.js
@@ -0,0 +1,119 @@
+import { NotFoundError } from "infra/errors";
+import { Orchestrator } from "tests/orchestrator";
+
+beforeAll(Orchestrator.prepare);
+
+describe("GET on /api/v1/users/[username]", () => {
+  describe("with Anonymous user", () => {
+    test("with exact case of username", async () => {
+      const userTest = {
+        username: "ExactCase",
+        email: "exact.case@email.com",
+        password: "qwerty",
+      };
+
+      const createTestUserRes = await fetch(
+        `${process.env.WEBSERVER_URL}/api/v1/users`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(userTest),
+        },
+      );
+
+      expect(createTestUserRes.status).toBe(201);
+
+      const findUserRes = await fetch(
+        `${process.env.WEBSERVER_URL}/api/v1/users/${userTest.username}`,
+        {
+          method: "GET",
+        },
+      );
+
+      expect(findUserRes.status).toBe(200);
+
+      const userFounded = await findUserRes.json();
+
+      expect(userFounded).toEqual(
+        expect.objectContaining({
+          created_at: expect.stringContaining(
+            new Date(userFounded.created_at).toISOString(),
+          ),
+          updated_at: expect.stringContaining(
+            new Date(userFounded.updated_at).toISOString(),
+          ),
+          id: expect.stringContaining(userFounded.id),
+          username: expect.stringContaining(userTest.username),
+          email: expect.stringContaining(userTest.email),
+          password: expect.stringContaining(userTest.password),
+        }),
+      );
+    });
+    test("with mismatch case of username", async () => {
+      const userTest = {
+        username: "MismatchCase",
+        email: "mismatch.case@email.com",
+        password: "qwerty",
+      };
+
+      const createTestUserRes = await fetch(
+        `${process.env.WEBSERVER_URL}/api/v1/users`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(userTest),
+        },
+      );
+
+      expect(createTestUserRes.status).toBe(201);
+
+      const findUserRes = await fetch(
+        `${process.env.WEBSERVER_URL}/api/v1/users/${userTest.username.toLowerCase()}`,
+        {
+          method: "GET",
+        },
+      );
+
+      expect(findUserRes.status).toBe(200);
+
+      const userFounded = await findUserRes.json();
+
+      expect(userFounded).toEqual(
+        expect.objectContaining({
+          created_at: expect.stringContaining(
+            new Date(userFounded.created_at).toISOString(),
+          ),
+          updated_at: expect.stringContaining(
+            new Date(userFounded.updated_at).toISOString(),
+          ),
+          id: expect.stringContaining(userFounded.id),
+          username: expect.stringContaining(userTest.username),
+          email: expect.stringContaining(userTest.email),
+          password: expect.stringContaining(userTest.password),
+        }),
+      );
+    });
+    test("with inexistent username", async () => {
+      const res = await fetch(
+        `${process.env.WEBSERVER_URL}/api/v1/users/UsuarioInexistente`,
+        {
+          method: "GET",
+        },
+      );
+
+      const errorBody = await res.json();
+
+      const expectedNotFoundError = new NotFoundError({
+        message: "Nenhum usuário encontrado para o username fornecido.",
+        action: "Tente buscar por outro username.",
+      });
+
+      expect(res.status).toBe(expectedNotFoundError.statusCode);
+      expect(errorBody).toEqual(expectedNotFoundError.toJSON());
+    });
+  });
+});
